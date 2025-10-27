65f204e18b36b4f47beb44db635434e361478c53
SAYYAM JAIN <sayyamjain@SAYYAMs-MacBook-Air.local>
2025-10-27 19:27:44 +0530

    Upgraded UI with animations and glass effect

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 from flask import Flask, render_template, request
-import requests
-import os
+import requests, os
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
-def home():
+def index():
     weather = None
     if request.method == "POST":
         city = request.form["city"]
@@ -17,4 +16,4 @@ def home():
     return render_template("index.html", weather=weather)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(debug=True)
